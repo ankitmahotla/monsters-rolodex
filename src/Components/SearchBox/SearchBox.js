eabd5d31dccc197258bd1aedfd3e223b67e6c182
ankitmahotla <ankitmahotla45@gmail.com>
2022-01-08 11:45:56 +0530

    Initial Commit

---
+++ src/Components/SearchBox/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SearchBox.styles.css';
+
+const SearchBox = ({placeholder, handleChange}) => {
+    return (
+        <div>
+            <input className='search'
+          type="search"
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+        </div>
+    )
+}
+
+export default SearchBox;
